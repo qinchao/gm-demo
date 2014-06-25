77a0c20c0efe69ebc22fbceaed9eb294274af2e1
Git-Admin <qinchao@gmsdtech.com>
2014-06-24 19:21:14 -0700

    Update controllers and entities.

--- app/controllers/com/gmsd/core/OrderController.java
+++ app/controllers/com/gmsd/core/OrderController.java
@@ -13,58 +13,21 @@ import play.mvc.Results;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+import java.util.Date;
 
 @Named
 @Singleton
 public class OrderController extends Controller {
 
-  public Result clientProducts() {
-    ArrayNode products = _getProductList();
-    return Results.ok(products);
+  public Result doOrder() {
+    return Results.forbidden("Unimplemented");
   }
 
-  /* Sample product list output
-  [
-    {
-      "id": "1",
-      "name": "product1",
-      "desc": "good!"
-    },
-    {
-      "id": "2",
-      "name": "product2",
-      "desc": "awesome!"
-    },
-    {
-      "id":"3",
-      "name":"product3",
-      "desc":"not bad"
-    }
-  ]
-   */
-  private ArrayNode _getProductList() {
-    final JsonNodeFactory factory = JsonNodeFactory.instance;
+  public Result queryOrder(Long id) {
+    return Results.forbidden("Unimplemented");
+  }
 
-    ObjectNode product1 = factory.objectNode();
-    product1.put("id", "1");
-    product1.put("name", "product1");
-    product1.put("desc", "good!");
-
-    ObjectNode product2 = factory.objectNode();
-    product2.put("id", "2");
-    product2.put("name", "product2");
-    product2.put("desc", "awesome!");
-
-    ObjectNode product3 = factory.objectNode();
-    product3.put("id", "3");
-    product3.put("name", "product3");
-    product3.put("desc", "not bad");
-
-    ArrayNode products = factory.arrayNode();
-    products.add(product1);
-    products.add(product2);
-    products.add(product3);
-
-    return products;
+  public Result queryOrdersByDate(Date startDate, Date endDate) {
+    return Results.forbidden("Unimplemented");
   }
 }
